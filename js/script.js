84fb3bf459bcdbdb85833c2272a944c64436c1d4
andreaa21 <andrea.zollini@proton.me>
2022-10-13 17:15:49 +0200

    bonus

--- js/script.js
+++ js/script.js
@@ -7,20 +7,30 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”. */
 const fizz = 'Fizz';
 const buzz = 'Buzz';
 const fizzBuzz = 'FizzBuzz';
+const limit = 100;
+const container = document.querySelector('.container');
 
-console.log('numeri da 1 a 100');
+for(let i = 1; i <= limit; i++){
 
-for(let i = 1; i <= 100; i++){
+    const box = document.createElement('div');
+    box.className ='box';
+    console.log(box);
+    box.innerHTML = i;
+    container.append(box)
 
     if(i % 3 === 0){
-        console.log(fizz);
+       box.innerHTML = fizz;
+       box.classList.add('red')
     }else if(i % 5 === 0){
-        console.log(buzz);
+        box.innerHTML = buzz;
+        box.classList.add('blue')
     }else{
         console.log(i);
+        box.classList.add('orange')
     }
 
     if(i % 5 == 0 && i % 3 == 0){
-        console.log(fizzBuzz);
+        box.innerHTML = fizzBuzz;
+        box.classList.add('yellow')
     }
 }
